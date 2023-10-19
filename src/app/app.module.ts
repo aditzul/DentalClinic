0aaea58e28aaaf2708eebc605411ed6fb37f2d27
Adrian Dragomiroiu <dragomiroiud@gmail.com>
2023-10-19 12:22:57 +0300

    Installed Material UI and added grid and cards to Dashboard page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,14 @@ import { BottomBarComponent } from './bottom-bar/bottom-bar.component';
 import { PageDashboardComponent } from './page-dashboard/page-dashboard.component';
 import { PageProfileComponent } from './page-profile/page-profile.component';
 import { PageLogoutComponent } from './page-logout/page-logout.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+
+
 
 @NgModule({
   declarations: [
@@ -22,7 +30,13 @@ import { PageLogoutComponent } from './page-logout/page-logout.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatGridListModule,
+    MatButtonModule,
+    MatInputModule,
+    MatIconModule,
+    MatCardModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/page-dashboard/page-dashboard.component.ts
+++ src/app/page-dashboard/page-dashboard.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
 
+export interface Tile {
+  color: string;
+  cols: number;
+  rows: number;
+  text: string;
+}
+
 @Component({
   selector: 'app-page-dashboard',
   templateUrl: './page-dashboard.component.html',
-  styleUrls: ['./page-dashboard.component.scss']
+  styleUrls: ['./page-dashboard.component.scss'],
 })
 export class PageDashboardComponent {
-
+  tiles: Tile[] = [
+    {text: 'One', cols: 2, rows: 1, color: 'lightblue'},
+    {text: 'Two', cols: 2, rows: 2, color: 'lightgreen'},
+    {text: 'Three', cols: 1, rows: 1, color: 'lightpink'},
+    {text: 'Four', cols: 1, rows: 1, color: '#DDBDF1'},
+  ];
 }
